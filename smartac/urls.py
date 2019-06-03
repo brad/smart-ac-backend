bc525270964a9f5b0dbbc4bd05918a0efb2476e4
Brad Pitcher <bradpitcher@gmail.com>
2019-06-02 20:21:49 -0600

    customize titles/headers in the admin

--- smartac/urls.py
+++ smartac/urls.py
@@ -3,13 +3,19 @@ from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import RedirectView
-from rest_framework.routers import DefaultRouter
-from .devices import views as device_views
 
 from allauth import urls as allauth_urls
 from invitations import urls as invitations_urls
+from rest_framework.routers import DefaultRouter
 
+from .devices import views as device_views
+
+
+admin.site.site_header = _('SmartAC Admin')
+admin.site.site_title = _('SmartAC Admin')
+admin.site.index_title = _('Welcome to the SmartAC Admin!')
 
 router = DefaultRouter()
 router.register('devices', device_views.DeviceCreateViewSet)
